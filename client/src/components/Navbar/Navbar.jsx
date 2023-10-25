1383bee329bbe81d22e3546a662976e3256cd5be
Radu-A <elcastravete@gmail.com>
2023-10-25 18:45:13 +0200

    working/auth0_login

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import { useAuth0 } from "@auth0/auth0-react";
 import homeworkIcon from "../../assets/icons/homework-icon.png";
 import DesktopMenu from "./DesktopMenu/DesktopMenu";
 import DropdownMenu from "./DropdownMenu/DropdownMenu";
 import SessionMenu from "./SessionMenu/SessionMenu";
+import LoginButton from "./LoginButton/LoginButton";
+import LogoutButton from "./LogoutButton/LogoutButton";
 
 const Navbar = () => {
   const isDesktop = useMediaQuery({
-    query: "(min-width: 825px)",
+    query: "(min-width: 920px)",
   });
   const [dropdownMenuClass, setDropdownMenuClass] = useState(
     "dropdown-menu-article"
   );
+  const { isAuthenticated } = useAuth0();
 
   return (
     <>
@@ -32,11 +36,18 @@ const Navbar = () => {
           )}
         </article>
         <article className="navbar-menu-article">
+          {isAuthenticated ?
+          <>
           {isDesktop && <DesktopMenu />}
           <SessionMenu
             dropdownMenuClass={dropdownMenuClass}
             setDropdownMenuClass={setDropdownMenuClass}
           />
+          <LogoutButton />
+          </>
+          :
+          <LoginButton />
+          }
         </article>
       </nav>
 
