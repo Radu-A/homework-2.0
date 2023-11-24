6579d207910cbb0ffff4a687f3505f87c96d2a68
Radu-A <elcastravete@gmail.com>
2023-11-24 11:20:33 +0100

    updated/redirect_uri

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,15 @@ import App from "./App.jsx";
 // import './index.css'
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const server = import.meta.env.VITE_SERVER;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: `http://localhost:5173/signup`,
+        redirect_uri: `${server}/signup`,
       }}
     >
       <App />
