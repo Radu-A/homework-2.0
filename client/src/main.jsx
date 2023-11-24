637ddfa21043a73fa71f24fb71617c89db79b7a5
Radu-A <elcastravete@gmail.com>
2023-11-24 13:04:10 +0100

    changed/redirect_uri

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: `${import.meta.env.VITE_CLIENT}/signup`,
+        redirect_uri: `${import.meta.env.VITE_CLIENT}`,
       }}
     >
       <App />
