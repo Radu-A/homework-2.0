61345d716f92460ad43ca0adadf73415f61c5d50
Radu-A <elcastravete@gmail.com>
2023-11-24 14:09:49 +0100

    fixed/logout_redirect

--- client/src/components/Navbar/LogoutButton/LogoutButton.jsx
+++ client/src/components/Navbar/LogoutButton/LogoutButton.jsx
@@ -3,7 +3,20 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return <button className="log-button" onClick={logout}>Log Out</button>;
+  const manageLogout = async () => {
+    const url = import.meta.env.VITE_CLIENT;
+    await logout({
+      openUrl() {
+        window.location.replace(url);
+      },
+    });
+  };
+
+  return (
+    <button className="log-button" onClick={manageLogout}>
+      Log Out
+    </button>
+  );
 };
 
 export default LogoutButton;
