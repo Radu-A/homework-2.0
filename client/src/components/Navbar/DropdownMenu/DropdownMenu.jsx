3f37873c7b08c5042b0666211bb8f33914744f6f
Radu-A <elcastravete@gmail.com>
2023-11-26 13:54:42 +0100

    styled/project_card_responsive

--- client/src/components/Navbar/DropdownMenu/DropdownMenu.jsx
+++ client/src/components/Navbar/DropdownMenu/DropdownMenu.jsx
@@ -4,28 +4,31 @@ import { Link } from "react-router-dom";
 
 const DropdownMenu = ({ dropdownMenuClass }) => {
   return (
-    <article className={dropdownMenuClass}>
-      <ul className="dropdown-menu-list">
-        <li className="dropdown-menu-item">
-          <Link to="/" className="dropdown-menu-link">
-            Community
-          </Link>
-        </li>
-        <li className="dropdown-menu-item">
-          <Link to="/dashboard" className="dropdown-menu-link">
-            Dashboard
-          </Link>
-        </li>
-        <li className="dropdown-menu-item">
-          <Link to="/profile" className="dropdown-menu-link">
-            Profile
-          </Link>
-        </li>
-      </ul>
-      <div className="signout-div">
-        <LogoutButton />
-      </div>
-    </article>
+    <>
+      <article className={dropdownMenuClass}>
+        <ul className="dropdown-menu-list">
+          <li className="dropdown-menu-item">
+            <Link to="/" className="dropdown-menu-link">
+              Community
+            </Link>
+          </li>
+          <li className="dropdown-menu-item">
+            <Link to="/dashboard" className="dropdown-menu-link">
+              Dashboard
+            </Link>
+          </li>
+          <li className="dropdown-menu-item">
+            <Link to="/profile" className="dropdown-menu-link">
+              Profile
+            </Link>
+          </li>
+        </ul>
+        <div className="signout-div">
+          <LogoutButton />
+        </div>
+      </article>
+      <div className="layer-div"></div>
+    </>
   );
 };
 
